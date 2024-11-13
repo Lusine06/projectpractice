a1bf9552547cef9d01fc6d39260f87200765c5d4
Lusine06 <lgasparyan144@gmail.com>
2024-11-13 18:20:07 +0400

    Fix cahnges

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Header from '../Header/Header'
+import Button from '../Button/Button'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <div>
+        <Header/>
+        <Button />
+    </div>
   )
 }
